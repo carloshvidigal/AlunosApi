4897e93f13cbf9aa0453ccc4d1aaa00a7160580b
chmov <chmov@PC-CARLOSHMV>
2026-04-03 21:52:45 -0300

    Add Aluno Model

--- AlunosApi/Models/Aluno.cs
+++ AlunosApi/Models/Aluno.cs
@@ -1,10 +1,22 @@
-﻿namespace AlunosApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlunosApi.Models
 {
     public class Aluno
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
         public string Email { get; set; }
+
+        [Required]
         public int Idade { get; set; }
 
 
