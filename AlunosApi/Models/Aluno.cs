83c1b2a21431f4996bac10d61e25de0515f38121
chmov <chmov@PC-CARLOSHMV>
2026-04-06 14:26:56 -0300

    Adiciona entidade Aluno

--- AlunosApi/Models/Aluno.cs
+++ AlunosApi/Models/Aluno.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AlunosApi.Models
 {
+    [Table("Alunos")]
     public class Aluno
     {
         [Key]
