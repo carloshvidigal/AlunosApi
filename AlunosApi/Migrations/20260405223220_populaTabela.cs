5ab447a0a9698b59606cbf080ba1ba0a0c4bfed9
chmov <chmov@PC-CARLOSHMV>
2026-04-06 14:26:28 -0300

    Cria migration inicial do banco e adiciona migration para popular tabela

---
+++ AlunosApi/Migrations/20260405223220_populaTabela.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AlunosApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class populaTabela : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "js@teste.com", 20, "Januário" },
+                    { 2, "couve@teste.com", 22, "Zé das Couves" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
