f9751e7540631835f058d2fbf25c5b3769769851
chmov <chmov@PC-CARLOSHMV>
2026-04-07 17:13:49 -0300

    feat: configura CORS para permitir requisições do localhost:3000

--- AlunosApi/Program.cs
+++ AlunosApi/Program.cs
@@ -26,6 +26,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options =>
+    options.WithOrigins("http://localhost:3000")
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+);
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
