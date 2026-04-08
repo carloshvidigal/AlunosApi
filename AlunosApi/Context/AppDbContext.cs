8f04cc408cdf867a8dc16eeb6c277c6fa1c19ca0
chmov <chmov@PC-CARLOSHMV>
2026-04-08 16:45:24 -0300

    Altera herança para IdentityDbContext

--- AlunosApi/Context/AppDbContext.cs
+++ AlunosApi/Context/AppDbContext.cs
@@ -1,20 +1,23 @@
 ﻿using AlunosApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AlunosApi.Context
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
         public DbSet<Aluno> Alunos { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Aluno>().HasData(
-                new Aluno { Id = 1, Nome = "Januário", Email = "js@teste.com", Idade = 20 },
-                new Aluno { Id = 2, Nome = "Zé das Couves", Email = "couve@teste.com", Idade = 22 });
-         }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Aluno>().HasData(
+        //        new Aluno { Id = 1, Nome = "Januário", Email = "js@teste.com", Idade = 20 },
+        //        new Aluno { Id = 2, Nome = "Zé das Couves", Email = "couve@teste.com", Idade = 22 });
+        //}
     }
 }
